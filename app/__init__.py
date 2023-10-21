3a6d02862d63cf72a44e05f50c01959b37fbdc7f
Jose Luis Verdugo <joseluisv36@gmail.com>
2023-10-20 21:25:21 -0300

    Metodos HTTP Aplicables

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST']) # Aca se indican los metodos permitidos, por defecto solo es GET
 def login():
-    return render_template('auth/login.html')
+    # print(request.method)
+    # print(request.form['usuario'])
+    # print(request.form['password'])
+    # NOTE: COn el metodos POST los datos del formaulario van en la URL, lo que no es seguro
+    if request.method == 'POST': # Si el metodo es post, entonces los datoss estan presentes y los puedo imprimir
+        print(request.form['usuario'])
+        print(request.form['password'])
+        return render_template('index.html')
+    else:
+        return render_template('auth/login.html')
 
 def pagina_no_encontrada(error):
     return render_template('errores/404.html'), 404 # El segundo es el codigo de error
