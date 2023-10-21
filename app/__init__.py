22824c5d9600ab5044283db6a8902c764c285daf
Jose Luis Verdugo <joseluisv36@gmail.com>
2023-10-20 21:32:41 -0300

    Lectura de valores enviados desde formularios

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,9 +12,12 @@ def login():
     # print(request.form['password'])
     # NOTE: COn el metodos POST los datos del formaulario van en la URL, lo que no es seguro
     if request.method == 'POST': # Si el metodo es post, entonces los datoss estan presentes y los puedo imprimir
-        print(request.form['usuario'])
-        print(request.form['password'])
-        return render_template('index.html')
+        # print(request.form['usuario'])
+        # print(request.form['password'])
+        if request.form['usuario'] == 'admin' and request.form['password'] == '123456':
+            return redirect(url_for('index'))
+        else:
+            return render_template('auth/login.html')
     else:
         return render_template('auth/login.html')
 
